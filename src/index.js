d4f5f8b22a01dd17a4c14db364eeadef3b846467
dedjer <dedjer@gmail.com>
2018-11-21 18:45:54 -0600

    MakeModelPage
    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import MakeModel from "./MakeModel.js";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
       <h1>JSON Array Object</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Load a JSON object into a dropdown.</h2>
+      <MakeModel />
     </div>
   );
 }
